e9b7380a38ad83aa04b71085ce58bed7916bbce5
lagomorph <raychengcx@live.com>
2020-08-04 17:57:06 +0900

    Unit selection completed

--- RaptorOCU/Assets/Scripts/Controllable/Unit.cs
+++ RaptorOCU/Assets/Scripts/Controllable/Unit.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 namespace Controllable
 {
-    public class Unit
+    public class Unit : MonoBehaviour
     {
         public Vector3 position;
         public Quaternion rotation;
         public string id;
+        public Status status;
+
+        public static Color beaconColor = new Color(0.41f, 1.00f, 0.96f);
+        public static Color payloadColor = new Color(0.00f, 0.82f, 1.00f);
+        public static Color focusedColor = Color.white;
+        public static Color focusInvColor = Color.black;
+
+        public virtual void SetSelectedColors(bool isSelected)
+        {
+            if (isSelected)
+            {
+                transform.GetChild(0).GetComponent<SpriteRenderer>().color = focusedColor;
+                transform.GetChild(1).GetComponent<TMPro.TextMeshPro>().color = focusInvColor;
+            }
+            else
+            {
+                transform.GetChild(0).GetComponent<SpriteRenderer>().color = focusInvColor;
+                transform.GetChild(1).GetComponent<TMPro.TextMeshPro>().color = focusedColor;
+            }
+        }
+        
+    }
+
+    public enum Status
+    {
+        Alive,
+        Dead
     }
 }
--- RaptorOCU/Assets/Scripts/OcuManager.cs
+++ RaptorOCU/Assets/Scripts/OcuManager.cs
@@ -1,18 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Controllable;
 
 public class OcuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private string _selectedUnit = null;
+    public string SelectedUnit
     {
-        
+        get { return _selectedUnit; }
+        set
+        {
+            //check previous selection then clear and reset buffers if any
+            if (_selectedUnit != null)
+            {
+                controllableUnits[_selectedUnit].SetSelectedColors(false);
+                IsManualControl = false;
+            }
+
+            if (value == null)
+            {
+                //close gui
+            }
+            else
+            {
+                //open and instantiate gui
+                controllableUnits[value].SetSelectedColors(true);
+            }
+            _selectedUnit = value;
+        }
     }
 
-    // Update is called once per frame
+    private bool _isManualControl = false;
+    public bool IsManualControl
+    {
+        get { return _isManualControl; }
+        set
+        {
+            //turn on or off gui
+            _isManualControl = value;
+        }
+    }
+
+    //public List<Unit> controllableUnits = new List<Unit>();
+    public Dictionary<string, Unit> controllableUnits = new Dictionary<string, Unit>();
+
+    /*Test Values--TO REMOVE ON PRODUCTION*/
+    float curSpeed = 10f;
+
+    void Start()
+    {
+        //populate controllableUnits list
+        Beacon b = GameObject.Find("beacon").GetComponent<Beacon>();
+        controllableUnits.Add(b.id, b);
+        //create units onscene
+    }
+
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                SelectedUnit = hit.collider.gameObject.GetComponent<Unit>().id;
+            }
+            else SelectedUnit = null;
+        }
+
+
+        if (SelectedUnit != null)
+        {
+            //movement controls
+            if (IsManualControl)
+            {
+                controllableUnits[SelectedUnit].GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
+                    Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
+            }
+        }
     }
 }
+
