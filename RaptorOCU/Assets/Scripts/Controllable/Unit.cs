f30994ee4f571a659399b833c7f86b241c9b21e7
lagomorph <raychengcx@live.com>
2020-08-04 14:21:19 +0900

    Basic scene setup

---
+++ RaptorOCU/Assets/Scripts/Controllable/Unit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Controllable
+{
+    public class Unit
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public string id;
+    }
+}
--- RaptorOCU/Assets/Scripts/OcuManager.cs
+++ RaptorOCU/Assets/Scripts/OcuManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MercatorProjection : MonoBehaviour
+public class OcuManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
