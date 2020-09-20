3f0c84e279da19002ee6fcadb5722987329edd5d
kujira <raychengcx@live.com>
2020-09-21 02:48:24 +0800

    ocu text logger

---
+++ RaptorOCU/Assets/Scripts/OcuLogItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OcuLogItem : MonoBehaviour
+{
+    public void SetText(string text, Color color)
+    {
+        GetComponent<Text>().text = text;
+        GetComponent<Text>().color = color;
+    }
+}
--- RaptorOCU/Assets/Scripts/OcuLogger.cs
+++ RaptorOCU/Assets/Scripts/OcuLogger.cs
@@ -4,21 +4,44 @@ using UnityEngine;
 
 public class OcuLogger : Singleton<OcuLogger>
 {
+    [SerializeField]
+    private GameObject textTemplate;
+    private List<GameObject> textItems = new List<GameObject>();
+
+
     //verbose
     public void Logv(string message)
     {
         print(message);
+        _LogText(message, Color.white);
     }
 
     //warning
     public void Logw(string message)
     {
         Debug.LogWarning(message);
+        _LogText(message, Color.yellow);
     }
 
     //error
     public void Loge(string message)
     {
         Debug.LogError(message);
+        _LogText(message, Color.red);
+    }
+
+    private void _LogText(string message, Color color)
+    {
+        if (textItems.Count >= 50)
+        {
+            GameObject tempText = textItems[0];
+            Destroy(tempText.gameObject);
+            textItems.Remove(tempText);
+        }
+        GameObject newText = Instantiate<GameObject>(textTemplate);
+        newText.SetActive(true);
+        newText.GetComponent<OcuLogItem>().SetText(message, color);
+        newText.transform.SetParent(textTemplate.transform.parent, false);
+        textItems.Add(newText.gameObject);
     }
 }
