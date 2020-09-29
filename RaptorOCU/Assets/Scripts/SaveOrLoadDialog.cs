7d4bc10bdc5a5c7048384268c4a6b99814870517
kujira <raychengcx@live.com>
2020-09-29 10:58:11 +0800

    file i/o save and load test scripts

---
+++ RaptorOCU/Assets/Scripts/SaveOrLoadDialog.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleFileBrowser;
+using System.IO;
+
+public class SaveOrLoadDialog : MonoBehaviour
+{
+	string res = "roarLab";
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        FileBrowser.SetFilters(true, new FileBrowser.Filter("Images", ".jpg", ".png"), new FileBrowser.Filter("Text Files", ".txt", ".pdf"));
+        FileBrowser.SetDefaultFilter(".jpg");
+        FileBrowser.SetExcludedExtensions(".lnk", ".tmp", ".zip", ".rar", ".exe");
+        FileBrowser.AddQuickLink("Users", "C:\\Users", null);
+        StartCoroutine(ShowSaveDialogCoroutine());
+
+    }
+
+	IEnumerator ShowSaveDialogCoroutine()
+	{
+		// Show a load file dialog and wait for a response from user
+		// Load file/folder: file, Allow multiple selection: true
+		// Initial path: default (Documents), Title: "Load File", submit button text: "Load"
+		yield return FileBrowser.WaitForSaveDialog(false, false, null, "Save File", "Save");
+
+		// Dialog is closed
+		// Print whether the user has selected some files/folders or cancelled the operation (FileBrowser.Success)
+		Debug.Log(FileBrowser.Success);
+
+		if (FileBrowser.Success)
+		{
+			string path = "";
+			// Print paths of the selected files (FileBrowser.Result) (null, if FileBrowser.Success is false)
+			for (int i = 0; i < FileBrowser.Result.Length; i++)
+			{ 
+				Debug.Log(FileBrowser.Result[i]);
+				path = FileBrowser.Result[i];
+			}
+			// Read the bytes of the first file via FileBrowserHelpers
+			// Contrary to File.ReadAllBytes, this function works on Android 10+, as well
+			byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(res);
+			File.WriteAllBytes(path, byteArray);
+		}
+	}
+
+	IEnumerator ShowLoadDialogCoroutine()
+	{
+		// Show a load file dialog and wait for a response from user
+		// Load file/folder: file, Allow multiple selection: true
+		// Initial path: default (Documents), Title: "Load File", submit button text: "Load"
+		yield return FileBrowser.WaitForLoadDialog(false, true, null, "Load File", "Load");
+
+		// Dialog is closed
+		// Print whether the user has selected some files/folders or cancelled the operation (FileBrowser.Success)
+		Debug.Log(FileBrowser.Success);
+
+		if (FileBrowser.Success)
+		{
+			// Print paths of the selected files (FileBrowser.Result) (null, if FileBrowser.Success is false)
+			for (int i = 0; i < FileBrowser.Result.Length; i++)
+				Debug.Log(FileBrowser.Result[i]);
+
+			// Read the bytes of the first file via FileBrowserHelpers
+			// Contrary to File.ReadAllBytes, this function works on Android 10+, as well
+			byte[] bytes = FileBrowserHelpers.ReadBytesFromFile(FileBrowser.Result[0]);
+			res = System.Text.Encoding.UTF8.GetString(bytes);
+			print(res);
+		}
+	}
+}
